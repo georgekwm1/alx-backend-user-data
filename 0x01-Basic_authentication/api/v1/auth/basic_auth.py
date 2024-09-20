48b6f18afc012f64dbef10b68a59dce1002b2a6d
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-20 18:24:40 +0100

    "Added import of base64 module and new method decode_base64_authorization_header to BasicAuth class in basic_auth.py"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
             return None
         # return authorization_header.split(" ")[1]
         return authorization_header.lstrip("Basic ")
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes base64 authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
