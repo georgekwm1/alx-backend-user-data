fa499f3f6f54bb6454fef4b20f98b6987faa168d
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-20 16:45:42 +0100

    "Added support for basic auth type, updated app.py to register auth based on AUTH_TYPE env variable"

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,10 +13,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
@@ -61,7 +65,3 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
-# elif auth_type == auth:
-#     from api.v1.auth.basic_auth import BasicAuth
-#     auth = BasicAuth()
