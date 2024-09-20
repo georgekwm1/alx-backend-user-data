23a29b74857ac9a8cb83c2c3d51b16db86b2bdaf
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-20 16:31:09 +0100

    "Added basic authentication to API v1, including auth type selection and authorization header checking"

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,27 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def check_user_auth():
+    if auth is None:
+        return
+    if auth.require_auth(request.path,
+                         ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']):
+
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -39,3 +60,7 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
+# elif auth_type == auth:
+#     from api.v1.auth.basic_auth import BasicAuth
+#     auth = BasicAuth()
