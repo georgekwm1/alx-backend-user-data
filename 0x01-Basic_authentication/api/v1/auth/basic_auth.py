841cccaa249a01a2ae1231e447da64aa01ec98d3
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-20 17:08:29 +0100

    "Added new method extract_base64_authorization_header to BasicAuth class in basic_auth.py and updated corresponding pyc file."

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth Class"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extracts base64 authorization header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        # return authorization_header.split(" ")[1]
+        return authorization_header.lstrip("Basic ")
