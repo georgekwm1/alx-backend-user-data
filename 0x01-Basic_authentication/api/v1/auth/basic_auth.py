214ae5e92ef521446c304372787bf2f8e4815f34
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-22 00:31:23 +0100

    "Refactored extract_user_credentials method in BasicAuth class to use regular expression for parsing user credentials from authorization header."

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,17 +35,35 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
+    # def extract_user_credentials(
+    #         self, decoded_base64_authorization_header: str) -> (str, str):
+    #     """Extracts user credentials"""
+    #     if decoded_base64_authorization_header is None:
+    #         return (None, None)
+    #     if not isinstance(decoded_base64_authorization_header, str):
+    #         return (None, None)
+    #     if ':' not in decoded_base64_authorization_header:
+    #         return (None, None)
+    #     a, b = decoded_base64_authorization_header.split(':')
+    #     return (a, b)
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
-        """Extracts user credentials"""
-        if decoded_base64_authorization_header is None:
-            return (None, None)
-        if not isinstance(decoded_base64_authorization_header, str):
-            return (None, None)
-        if ':' not in decoded_base64_authorization_header:
-            return (None, None)
-        a, b = decoded_base64_authorization_header.split(':')
-        return (a, b)
+            self,
+            decoded_base64_authorization_header: str,
+    ) -> Tuple[str, str]:
+        """Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow.
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
