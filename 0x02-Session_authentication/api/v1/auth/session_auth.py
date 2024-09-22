74407d92380593773ca502e71d74dc04e91b1b14
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-22 12:48:30 +0100

    "Added create_session method to SessionAuth class and initialized user_id_by_session_id dictionary"

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,19 @@
 """1. Empty session"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
