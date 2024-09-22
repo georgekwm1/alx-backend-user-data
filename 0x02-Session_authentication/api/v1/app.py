8af2726d4e3c6c01cd60c4e9bfd8d6a477fb8e0b
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-22 17:15:47 +0100

    "Refactor auth checks in app.py, reorder and remove unnecessary lines"

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,15 +36,14 @@ def check_user_auth():
                       '/api/v1/forbidden/',
                       '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
-
         if auth.authorization_header(request) is None:
             abort(401)
-
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
+            abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        if auth.authorization_header(request) is None and \
-                auth.current_user(request) is None:
-            abort(401)
+
         request.current_user = auth.current_user(request)
 
 
