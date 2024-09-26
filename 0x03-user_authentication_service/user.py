b07a9ad0910f73ef032f85bdd39e006b2a01915c
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-26 21:20:54 +0100

    "Removed import of sqlalchemy, added import of create_engine from sqlalchemy"

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 
 """User Module"""
 
-import sqlalchemy
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
