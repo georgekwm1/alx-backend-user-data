fa4c53a0db0e55249f85214e8ff461d078e3422f
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-26 21:11:43 +0100

    created user.py

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""User Module"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+engine = create_engine('sqlite:///app.db', echo=False)
+Base = declarative_base()
+
+
+class User(Base):
+    """A base class for users."""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
+Base.metadata.create_all(engine)
