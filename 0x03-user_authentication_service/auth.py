19c71695b97baec0dd5ec49c90570c9d0c3e91b5
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-28 12:17:04 +0100

    "Removed self parameter from _generate_uuid method"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,6 +39,6 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid(self) -> str:
+    def _generate_uuid() -> str:
         """Generates a UUID"""
         return str(uuid.uuid4())
