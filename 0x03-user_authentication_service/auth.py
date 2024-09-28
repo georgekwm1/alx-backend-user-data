87c691070f61a47f54f6391738acd0e1961de002
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-28 08:00:17 +0100

    "Refactored register_user method in Auth class to use try-except block instead of if-else statement"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,12 +19,20 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    # def register_user(self, email: str, password: str) -> User:
+    #     """Registers a new user"""
+    #     hashed_password = _hash_password(password)
+    #     if self._db._session.query(User).filter_by(email=email).first() \
+    #             is not None:
+    #         raise ValueError(f"User {email} already exists")
+    #     user = self._db.add_user(email, hashed_password)
+    #     self._db.commit()
+    #     return user
     def register_user(self, email: str, password: str) -> User:
-        """Registers a new user"""
-        hashed_password = _hash_password(password)
-        if self._db._session.query(User).filter_by(email=email).first() \
-                is not None:
-            raise ValueError(f"User {email} already exists")
-        user = self._db.add_user(email, hashed_password)
-        self._db.commit()
-        return user
+        """Adds a new user to the database.
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
